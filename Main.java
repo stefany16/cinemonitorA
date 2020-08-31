9c6d60c648adfa4354cdc0fe0b46439df09fb339
Stefany Silva <stefanysilvaoliver16@gmail.com>
2020-08-31 20:16:43 +0000

    Class Main

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Filme filme = new Filme();
+
+    filme.setTitulo("Barraca do beijo");
+    filme.setTipo("Romance");
+    filme.setAno(2020);
+    
+
+    System.out.println("Filme: " + filme.getTitulo());
+    System.out.println("Ano: " + filme.getAno());
+    System.out.println("Tipo: " + filme.getTipo());
+
   }
 }
---
+++ filme.java
@@ -0,0 +1,30 @@
+public class filme{
+
+  private String titulo;
+  private int ano;
+  private String tipo;
+
+  public void setTitulo(String titulo){
+    this.titulo = titulo;
+  }
+
+  public void setAno(int ano){
+    this.ano = ano;
+  }
+
+  public void setTipo(String tipo){
+    this.tipo = tipo;
+  }
+
+  public String getTitulo(){
+    return this.titulo;
+  }
+
+  public int getAno(){
+    return this.ano;
+  }
+
+  public String getTipo(){
+    return this.tipo;
+  }
+}
